ded965aa8d9b94c3588cea5c982ae64f3c350999
AkulinIvan <ivanakulin175@gmail.com>
2025-02-20 08:17:46 +0700

    commit

--- routes/routes.go
+++ routes/routes.go
@@ -1,25 +1,25 @@
-// package routes
+package routes
 
-// import (
-// 	controller "github.com/AkulinIvan/go-test/controller"
-// 	"github.com/gofiber/fiber/v3"
-// )
+import (
+	controller "github.com/AkulinIvan/go-test/controller"
+	"github.com/gofiber/fiber/v3"
+)
 
-// func Setup(c *fiber.Ctx) {
-// 	// Создаем новый экземпляр Fiber
-// 	app := fiber.New()
+func Setup(app *fiber.App) {
+	// Создаем новый экземпляр Fiber
+	
 
-// 	// Создаем новую задачу
-// 	app.Post("/tasks/", controller.CreateTask)
+	// Создаем новую задачу
+	app.Post("/tasks/", controller.CreateTask)
+	//Получаем список всех задач
+	app.Get("/tasks", controller.ListOfTasks)
+}
 
-// }
+// 	controller.CreateTask})
 
-// // 	controller.CreateTask})
-// // //Получаем список всех задач
-// // app.Get("/tasks/", controller.ListOfTasks)
-// // //Обновляем/изменяем задачу
-// // app.Put("/tasks/:id", controller.UpdateTask)
-// // //Удаляем задачу
-// // app.Delete("/tasks/:id", controller.DeleteTask)
-// //Слушаем приложение на порту 3000
-// // app.Listen(":3000")
+// //Обновляем/изменяем задачу
+// app.Put("/tasks/:id", controller.UpdateTask)
+// //Удаляем задачу
+// app.Delete("/tasks/:id", controller.DeleteTask)
+//Слушаем приложение на порту 3000
+// app.Listen(":3000")
